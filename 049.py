62de8e32d5caa2c4da8efb35f84f268231c53c57
askostin <askostin.mipt@yandex.ru>
2023-01-08 21:45:20 +0300

    add generation of all possible permutations of prime 4-digit numbers, need to select lists of 3 numbers, which form arithmetic sequence

--- 049.py
+++ 049.py
@@ -13,13 +13,14 @@
 # the three terms in this sequence?
 
 from numtheory import primes, dividers
+from algo import comb
 
 # This array is for only two different groups of digits,
 # where all permutations of all digits from each group
 # form only prime 4-digits numbers.
 # (It's known fact that there are only two such groups).
 
-answer = [[0]*3]*2
+answer = []
 
 # Counter of the solutions registered in @answer.
 
@@ -32,30 +33,20 @@ temp = []
 
 # Step 1: Build the list of all 4-digits prime numbers.
 
-all_primes = [x for x in primes.primes_list(9991) if x > 1000]
-
-# Step 2: Sequentally take prime numbers from list and check set of digits
-# of each number if there are at least two prime numbers, formed by permutations
-# of these digits (and they have to be in the list of primes):
-# - if "yes" -- write list of these permutations in @array;
-# - if "no", remove from @all_primes the number itself and, if exists,
-# the only premutations of its digits, forming prime number.
+primes_4d = [x for x in primes.primes(9991) if x > 1000]
 
 def prime_permutations(p_list):
-    if p_list:
-        temp = [x for x in p_list if ((x%2 != 0) and (not (x in p_list)))]
-        if len(temp) > 2:
-            for i in (0, 3):
-                answer[n][i] = temp[i]
-            n += 1
-            if n == 2:
-                return answer
-        else:
-            # remove all elements of @temp from the p_list
-            for i in temp:
-                p_list.remove(i)
-            prime_permutations(p_list)
-    else:
-        return answer
+	if not p_list:
+		return
+	p = p_list[0]
+	p_perms = [x for x in comb.permutations(p) if ((x%2!=0) and (x in p_list))]
+	if len(p_perms) > 2:
+		p_perms.sort()
+		for i in range(len(p_perms)-2):
+			if (p_perms[i+2] - p_perms[i+1]) == (p_perms[i+1] - p_perms[i]):
+				print(p_perms)
+				answer.append(p)
+	prime_permutations([x for x in p_list if (x not in p_perms)])
 
-#print(prime_permutations(all_primes))
+prime_permutations(primes_4d)
+print(answer)
